e35504904a4783a7c46285675438744012d17871
CedricYonta <ecyontay01monccnb.ca>
2022-11-25 14:29:59 -0400

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 
 
-#include <Arduino.h>
+/*#include <Arduino.h>
 
 
 const int analogInPin = A1; 
@@ -46,4 +46,30 @@ if ((presenttime-lasttime)  > STARTTIME ) // temps d'activation
   lasttime = millis();
 }
 
+}*/
+
+#include <Arduino.h>
+const int BOUTTON_LED_PIN = 5;
+const int RELAIS = 6;
+int ETAT_LED;
+
+void allume()
+{
+    ETAT_LED = !ETAT_LED;
+}
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    pinMode(BOUTTON_LED_PIN, INPUT);
+    pinMode(RELAIS, OUTPUT);
+
+    attachInterrupt(digitalPinToInterrupt(BOUTTON_LED_PIN), allume, CHANGE);
+}
+
+void loop()
+{
+
+    digitalWrite(RELAIS, ETAT_LED);
 }
